1c69060c2feb0fbcf519c4b0bfc23ea2f09af1c8
Ola-jed <olabijed@gmail.com>
2024-07-03 22:09:57 +0100

    Init

---
+++ src/main/kotlin/bj/ifri/mlcontestapi/config/GlobalExceptionHandler.kt
@@ -0,0 +1,17 @@
+package bj.ifri.mlcontestapi.config
+
+import org.springframework.http.HttpStatus
+import org.springframework.validation.FieldError
+import org.springframework.web.bind.MethodArgumentNotValidException
+import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.RestControllerAdvice
+
+@RestControllerAdvice
+class GlobalExceptionHandler {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException::class)
+    fun handleValidationExceptions(ex: MethodArgumentNotValidException): Map<String, String?> {
+        return ex.bindingResult.allErrors.associateBy({ (it as FieldError).field }, { it.defaultMessage })
+    }
+}
---
+++ src/test/kotlin/bj/ifri/mlcontestapi/MlcontestApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package bj.ifri.mlcontestapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MlcontestApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
