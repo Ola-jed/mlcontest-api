b37f30e3088779070bf50dfa78767aca2dd11b84
Ola-jed <olabijed@gmail.com>
2024-07-05 14:50:16 +0100

    Improved error handling to return meaningful http codes for unknown errors

--- src/main/kotlin/bj/ifri/mlcontestapi/config/GlobalExceptionHandler.kt
+++ src/main/kotlin/bj/ifri/mlcontestapi/config/GlobalExceptionHandler.kt
@@ -1,6 +1,8 @@
 package bj.ifri.mlcontestapi.config
 
 import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.security.core.AuthenticationException
 import org.springframework.validation.FieldError
 import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
@@ -13,5 +15,20 @@ class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException::class)
     fun handleValidationExceptions(ex: MethodArgumentNotValidException): Map<String, String?> {
         return ex.bindingResult.allErrors.associateBy({ (it as FieldError).field }, { it.defaultMessage })
+    }
+
+    @ExceptionHandler(AccessDeniedException::class)
+    fun handleAccessDeniedException(ex: AccessDeniedException): ResponseEntity<String> {
+        return ResponseEntity("Access Denied: ${ex.message}", HttpStatus.FORBIDDEN)
+    }
+
+    @ExceptionHandler(AuthenticationException::class)
+    fun handleAuthenticationException(ex: AuthenticationException): ResponseEntity<String> {
+        return ResponseEntity("Authentication Failed: ${ex.message}", HttpStatus.UNAUTHORIZED)
+    }
+
+    @ExceptionHandler(Exception::class)
+    fun handleGeneralException(ex: Exception): ResponseEntity<String> {
+        return ResponseEntity("Internal Server Error: ${ex.message}", HttpStatus.INTERNAL_SERVER_ERROR)
     }
 }
